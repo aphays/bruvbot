438ef7aa626f09e1df37081f83c2b61752d480f1
aphays <aidanphays@gmail.com>
2020-10-05 13:54:03 -0400

    fixed stuff, added a response or two

--- responses.js
+++ responses.js
@@ -1,7 +1,8 @@
+// anything that could be deemed offensive is purely satirical and not intended to harm others
 const Discord = require('discord.js');
 
 module.exports = {
-  respondToMessage: function(message, phrase, response) {
+  respondToMessage: function(message, content) {
 
     if (message.content.includes("hiii")) {
       message.channel.send("hi :3");
@@ -12,19 +13,27 @@ module.exports = {
     if (message.content.includes("!!!")) {
       message.channel.send("❗❗❗"); 
     }}{
-      if (message.content.includes("women")) {
-        message.channel.send("https://media.discordapp.net/attachments/691118711486152705/761283953729863700/Eg-B1hwXcAAHguZ.jpg?width=490&height=524"); 
-    }}{
-      if (message.content.includes("woman")) {
+      if (message.content.startsWith("women") || message.content.startsWith("woman")) {
         message.channel.send("https://media.discordapp.net/attachments/691118711486152705/761283953729863700/Eg-B1hwXcAAHguZ.jpg?width=490&height=524"); 
     }}
   }
 },
 respondToDM: function(message, phrase, response) {
     
-  if (message.channel instanceof Discord.DMChannel) {
-    if(message.content.includes("hey"))
+  if (message.channel instanceof Discord.DMChannel) 
+  {
+    if(message.content.startsWith("hey"))
     message.channel.send("hi :3");
+  }
+  if (message.channel instanceof Discord.DMChannel) 
+  {
+    if(message.content.includes("show feet"))
+    message.channel.send("https://media.discordapp.net/attachments/691118711486152705/761362767503818782/face.png");
+  }
+  if (message.channel instanceof Discord.DMChannel) 
+  {
+    if(message.content.includes("suna"))
+    message.channel.send("love that guy 😍😍😍😍");
   }
 }
 }
